e519e478d13d00725307a2fb538fd37c4c1cfaba
RogerCastillo877 <roger.castillo877@gmail.com>
2025-10-18 13:58:56 -0500

    feat: implement debounce functionality in search input and add caching for country search queries

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interface';
-import { catchError, map, Observable, throwError } from 'rxjs';
+import { catchError, count, map, Observable, of, tap, throwError } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mapper';
 
@@ -13,13 +13,20 @@ const API_URL = 'https://restcountries.com/v3.1';
 export class CountryService {
 
   private http = inject(HttpClient);
+  private _qeuryCacheCapital = new Map<string, Country[]>();
+  private _qeuryCacheCountry = new Map<string, Country[]>();
 
   searchByCapital(query: string): Observable<Country[]> {
     query = query.toLowerCase().trim();
 
+    if( this._qeuryCacheCapital.has( query ) ) {
+      return of( this._qeuryCacheCapital.get( query ) ?? [] );
+    }
+
     return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`)
       .pipe(
         map( restCountries => CountryMapper.mapRestCountryArrayToCountryArray(restCountries) ),
+        tap( countries => this._qeuryCacheCapital.set( query, countries ) ),
         catchError( err => {
           console.log('Error en el servicio', err);
           return throwError(() => new Error(`No se pudo obtener países con ese query ${query}`));
@@ -30,9 +37,14 @@ export class CountryService {
   searchByCountry(query: string): Observable<Country[]> {
     query = query.toLowerCase().trim();
 
+    if( this._qeuryCacheCountry.has( query ) ) {
+      return of( this._qeuryCacheCountry.get( query ) ?? [] );
+    }
+
     return this.http.get<RESTCountry[]>(`${API_URL}/name/${query}`)
       .pipe(
         map( restCountries => CountryMapper.mapRestCountryArrayToCountryArray(restCountries) ),
+        tap( countries => this._qeuryCacheCountry.set( query, countries ) ),
         catchError( err => {
           console.log('Error en el servicio', err);
           return throwError(() => new Error(`No se pudo obtener países con ese query ${query}`));
